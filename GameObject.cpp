3f1d094571164f25f95d0f31b76bd9f581f8512e
unknown <alexwood319@gmail.com>
2023-12-30 11:44:07 -0800

    Added gameobject cpp file

---
+++ GameObject.cpp
@@ -0,0 +1,46 @@
+#include "GameObject.h"
+
+void GameObject::EarlyStart()
+{
+
+}
+
+void GameObject::Start()
+{
+
+}
+
+void GameObject::LateStart()
+{
+
+}
+
+void GameObject::EarlyUpdate(float DeltaTime)
+{
+	
+}
+
+void GameObject::Update(float DeltaTime)
+{
+
+}
+
+void GameObject::LateUpdate(float DeltaTime)
+{
+
+}
+
+void GameObject::EarlyDraw()
+{
+
+}
+
+void GameObject::Draw()
+{
+
+}
+
+void GameObject::LateDraw()
+{
+
+}
--- GameObject.h
+++ GameObject.h
@@ -1,6 +1,21 @@
 #pragma once
+#include "Ray.hpp"
 
 class GameObject
 {
+public:
+	Vector2 Position;
+	float Rotation;
 
+	virtual void EarlyStart();
+	virtual void Start();
+	virtual void LateStart();
+
+	virtual void EarlyUpdate(float DeltaTime);
+	virtual void Update(float DeltaTime);
+	virtual void LateUpdate(float DeltaTime);
+
+	virtual void EarlyDraw();
+	virtual void Draw();
+	virtual void LateDraw();
 };
